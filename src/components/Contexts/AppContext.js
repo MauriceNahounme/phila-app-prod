534f99b60ac73e21c8e8252a1a3956dfab8d0a1b
MauriceNahounme <mauricenahounme444@gmail.com>
2023-02-11 03:16:56 +0100

    m

--- src/components/Contexts/AppContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const uidContext = createContext();
-
-export const adminContext = createContext();
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./user.reducer";
-import memberReducer from "./member.reducer";
-
-export default combineReducers({
-  userReducer,
-  memberReducer,
-});
