654d75380e8fbba9668ad5139e58f47c41aae2aa
MauriceNahounme <mauricenahounme444@gmail.com>
2023-02-11 07:17:53 +0100

    m

--- src/components/Authentification/Member/Login.js
+++ src/components/Authentification/Member/Login.js
@@ -17,10 +17,14 @@ const Login = () => {
     e.preventDefault();
 
     axios
-      .post(`${BASE_URL}/members/login`, {
-        email,
-        password,
-      })
+      .post(
+        `${BASE_URL}/members/login`,
+        JSON.stringify({
+          email,
+          password,
+        }),
+        { "Access-Control-Allow-Credentials": true }
+      )
       .then((res) => {
         localStorage.setItem("tokenMember", res.data.tokenMember);
         authHeader(res.data.tokenMember);
